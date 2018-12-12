3da58781435b8b160a730a978c52eedc6e9331c6
sivasai-mudigonda <sivasai-mudigonda@github.com>
2018-12-11 22:45:22 -0800

    // Java sol for career cup-Arrays-Chap-1
    // CStyle String Reverse
    // Remove Duplicate char from String.
    // Replace String
    // String Anagram Check
    // String Unique Char Check

---
+++ javapractice/src/javapractice/ReplaceString.java
@@ -0,0 +1,42 @@
+package javapractice;
+
+public class ReplaceString {
+
+	public static void main(String[] args) {
+		String s = "Hello WOR LD WELCOME ON E";
+		String str = replaceString(s);
+		System.out.println(str);
+
+		str = replaceStringStringBuilder(s);
+		System.out.println(str);
+	}
+
+	// Using replaceAll()
+	private static String replaceString(String str) {
+		if (str == null || str.isEmpty()) {
+			return "";
+		}
+		String output = new String();
+		output = str.replaceAll("\\s", "%20");
+		return output;
+	}
+
+	// Using String Builder
+	private static String replaceStringStringBuilder(String str) {
+		if (str == null || str.isEmpty()) {
+			return "";
+		}
+		StringBuilder sb = new StringBuilder();
+		char[] charArr = str.toCharArray();
+		for (char c : charArr) {
+			if (c == ' ') {
+				sb.append("%20");
+			} else {
+				sb.append(c);
+			}
+		}
+
+		return sb.toString();
+	}
+
+}
