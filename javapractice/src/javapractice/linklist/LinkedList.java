cffaf6c881f6b1d8ba7ccd193d3eb6218495d70e
sivasai-mudigonda <sivasai-mudigonda@github.com>
2018-12-20 00:21:10 -0800

    // Find Start Pt of Circular Linked List
    // Find Start Pt of Circular Linked List

--- javapractice/src/javapractice/linklist/LinkedList.java
+++ javapractice/src/javapractice/linklist/LinkedList.java
@@ -2,29 +2,37 @@ package javapractice.linklist;
 
 public class LinkedList {
 	Node head;
-	
-	public void printList(Node node){
-		while(node!=null) {
+
+	public void printList(Node node) {
+		while (node != null) {
 			System.out.println(node.data);
-			node=node.next;
+			node = node.next;
 		}
 	}
 	
+	// To get Last Node in Linked List.
+	public Node getLastNode() {
+		while (head.next != null) {
+			head = head.next;
+		}
+		return head;
+	}
+
 	// Insert Integer Elements in to Linked List.
 	public void push(Object[] valLst) {
 		boolean isFirstEle = false;
 		Node tempNode = null;
-		for(Object val:valLst) {
-			Node newNode= new Node((Integer)val);
-			if(tempNode==null && !isFirstEle) {
-				tempNode=newNode;
-				head=tempNode;
-				isFirstEle=true;
+		for (Object val : valLst) {
+			Node newNode = new Node((Integer) val);
+			if (tempNode == null && !isFirstEle) {
+				tempNode = newNode;
+				head = tempNode;
+				isFirstEle = true;
 				continue;
 			}
-			if(tempNode!=null) {
-				tempNode.next=newNode;
-				tempNode=tempNode.next;
+			if (tempNode != null) {
+				tempNode.next = newNode;
+				tempNode = tempNode.next;
 			}
 		}
 	}
