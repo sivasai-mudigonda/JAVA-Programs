edadc675ec741c6a462bd3a953995c7ef179a6d7
sivasai-mudigonda <42163871+sivasai-mudigonda@users.noreply.github.com>
2019-09-17 20:33:53 -0700

    // Update MatrixSetZeros class.

--- javapractice/src/arrays/MatrixSetZeros.java
+++ javapractice/src/arrays/MatrixSetZeros.java
@@ -29,57 +29,57 @@ public class MatrixSetZeros {
 	}
 
 	// No Extra space
-	private static void setZeros(int[][] mat) {
+	private static void setZeros(int[][] matrix) {
 		boolean row_flag = false;
 		boolean col_flag = false;
 
-		for (int i = 0; i < mat.length; i++) {
-			for (int j = 0; j < mat[0].length; j++) {
-				if (i == 0 && mat[i][j] == 0) {
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[0].length; j++) {
+				if (i == 0 && matrix[i][j] == 0) {
 					row_flag = true;
 				}
-				if (j == 0 && mat[i][j] == 0) {
+				if (j == 0 && matrix[i][j] == 0) {
 					col_flag = true;
 				}
-				if (mat[i][j] == 0) {
-					mat[0][j] = 0;
-					mat[i][0] = 0;
+				if (matrix[i][j] == 0) {
+					matrix[0][j] = 0;
+					matrix[i][0] = 0;
 				}
 			}
 		}
 
-		for (int i = 1; i < mat.length; i++) {
-			for (int j = 1; j < mat[0].length; j++) {
-				if (mat[0][j] == 0 || mat[i][0] == 0) {
-					mat[i][j] = 0;
+		for (int i = 1; i < matrix.length; i++) {
+			for (int j = 1; j < matrix[0].length; j++) {
+				if (matrix[0][j] == 0 || matrix[i][0] == 0) {
+					matrix[i][j] = 0;
 				}
 			}
 		}
 
 		if (row_flag == true) {
-			for (int i = 0; i < mat[0].length; i++) {
-				mat[0][i] = 0;
+			for (int i = 0; i < matrix[0].length; i++) {
+				matrix[0][i] = 0;
 			}
 		}
 
 		if (col_flag == true) {
-			for (int i = 0; i < mat.length; i++) {
-				mat[i][0] = 0;
+			for (int i = 0; i < matrix.length; i++) {
+				matrix[i][0] = 0;
 			}
 		}
 	}
 
 	// Using extra space.
-	private static void setZerosExtraSpace(int[][] mat) {
-		int[] row = new int[ROWS];
+	private static void setZerosExtraSpace(int[][] matrix) {
+		int[] rows = new int[ROWS];
 		int[] colums = new int[COLUMS];
-		Arrays.fill(row, 1);
+		Arrays.fill(rows, 1);
 		Arrays.fill(colums, 1);
 
 		for (int i = 0; i < ROWS; i++) {
 			for (int j = 0; j < COLUMS; j++) {
-				if (mat[i][j] == 0) {
-					row[i] = 0;
+				if (matrix[i][j] == 0) {
+					rows[i] = 0;
 					colums[j] = 0;
 
 				}
@@ -88,17 +88,17 @@ public class MatrixSetZeros {
 
 		for (int i = 0; i < ROWS; i++) {
 			for (int j = 0; j < COLUMS; j++) {
-				if (row[i] == 0 || colums[j] == 0) {
-					mat[i][j] = 0;
+				if (rows[i] == 0 || colums[j] == 0) {
+					matrix[i][j] = 0;
 				}
 			}
 		}
 	}
 
-	private static void printMatrix(int[][] mat) {
+	private static void printMatrix(int[][] matrix) {
 		for (int i = 0; i < ROWS; i++) {
 			for (int j = 0; j < COLUMS; j++) {
-				System.out.print("\t" + mat[i][j]);
+				System.out.print("\t" + matrix[i][j]);
 			}
 			System.out.println();
 		}
