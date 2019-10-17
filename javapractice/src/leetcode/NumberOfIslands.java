01092a5dcee371da6b8bd0aa1b90483836fc0356
sivasai-mudigonda <42163871+sivasai-mudigonda@users.noreply.github.com>
2019-10-16 20:46:42 -0700

    // Update NumberOfIslands

--- javapractice/src/leetcode/NumberOfIslands.java
+++ javapractice/src/leetcode/NumberOfIslands.java
@@ -33,8 +33,8 @@ public class NumberOfIslands {
                     	    	{ 0, 1, 0, 0, 1 }, 
                     	    	{ 1, 0, 0, 1, 1 }, 
                     	    	{ 0, 0, 0, 0, 0 },
-                    		{ 1, 0, 1, 0, 1 } 
-                    	    	};
+                    		    { 1, 0, 1, 0, 1 } 
+                    	    };
 	System.out.println("Number of Islands = " +calClusterCnt(M)); // Expected Output = 6
 
 	/*
