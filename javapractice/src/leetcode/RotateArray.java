bc1e82d273a435f7c35125c9ab418b6632073fc3
sivasai-mudigonda <42163871+sivasai-mudigonda@users.noreply.github.com>
2019-10-16 19:03:32 -0700

    // Update RotateArray

--- javapractice/src/leetcode/RotateArray.java
+++ javapractice/src/leetcode/RotateArray.java
@@ -1,69 +1,108 @@
 package leetcode;
 
+/**
+*
+*/
+import java.util.Arrays;
 
-// Refer https://www.geeksforgeeks.org/array-rotation/
-
+/**
+ * @author u230107
+ *
+ *         LeetCode Ques - 189 {Rotate Array}
+ *         https://leetcode.com/problems/rotate-array/
+ *
+ *         Given an array, rotate the array to the right by k steps, where k is
+ *         non-negative.
+ *
+ *         Example 1: Input: [1,2,3,4,5,6,7] and k = 3 Output: [5,6,7,1,2,3,4]
+ *         Explanation: rotate 1 steps to the right: [7,1,2,3,4,5,6] rotate 2
+ *         steps to the right: [6,7,1,2,3,4,5] rotate 3 steps to the right:
+ *         [5,6,7,1,2,3,4]
+ * 
+ *         Example 2: Input: [-1,-100,3,99] and k = 2 Output: [3,99,-1,-100]
+ *         Explanation: rotate 1 steps to the right: [99,-1,-100,3] rotate 2
+ *         steps to the right: [3,99,-1,-100]
+ *
+ *         Note: Try to come up as many solutions as you can, there are at least
+ *         3 different ways to solve this problem. Could you do it in-place with
+ *         O(1) extra space?
+ *
+ *         Time Complexity = O(N) Space Complexity =o(1)
+ */
 public class RotateArray {
 
-	public static void main(String[] args) {
-		RotateArray rotate = new RotateArray();
-        int arr[] = {1, 2, 3, 4, 5, 6, 7,8,9,10,11,12};
-        rotate.leftRotate(arr, 5, 12);
-        rotate.printArray(arr, 12);
-	}
-	
-	
-	/*
-	 * GCD{arr.length,d}={12,3}=3. So, 3 sets
-	 * Sets: {1,4,7,10}
-	 *       {2,5,8,11}
-	 *       {3,6,9,12}
-	 */
-	
-	void leftRotate(int arr[], int d, int n) 
-    {
-        int i, j, k, temp;
-        for (i = 0; i < gcd(d, n); i++)  // To Divide in to gcd number of sets.
-        {
-            /* move i-th values of blocks */
-            temp = arr[i];
-            j = i;
-            while (1 != 0)  // Internal set element rotation by d locations.
-            {
-                k = j + d;
-                if (k >= n) 
-                    k = k - n;
-                if (k == i) 
-                    break;
-                arr[j] = arr[k];
-                j = k;
-            }
-            arr[j] = temp; // replace first element of set back in the set in the last loc.
-        }
-    }
-	
-	/*Fuction to get gcd of a and b*/
-	/*
-	 * 12 is divided by 1,3,4,6,12
-	 * 3 is divided by 1,3
-	 * 
-	 * Greatest common divisor is 3
-	 */
-	
-    int gcd(int a, int b) 
-    {
-        if (b == 0)
-            return a;
-        else
-            return gcd(b, a % b);
-    }
-    
-    /* function to print an array */
-    void printArray(int arr[], int size) 
-    {
-        int i;
-        for (i = 0; i < size; i++)
-            System.out.print(arr[i] + " ");
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+	RotateArray obj = new RotateArray();
+	System.out.println("*******Right Rotation********");
+	// Right Rotation
+	int arr[] = { 1, 2, 3, 4, 5, 6, 7 };
+	int k = 3;
+	obj.rotate(arr, k);
+	System.out.println("After Right Rotation : " + Arrays.toString(arr)); // Expected Output = 5 6 7 1 2 3 4
+
+	int arr_2[] = { -1, -100, 3, 99 };
+	k = 2;
+	obj.rotate(arr_2, k);
+	System.out.println("After Right Rotation : " + Arrays.toString(arr_2)); // Expected Output = 3 99 -1 -100
+
+	System.out.println("********Left Rotation********");
+	// Left Rotation
+	int arr_3[] = { 1, 2, 3, 4, 5, 6, 7 };
+	k = 3;
+	obj.leftRotate(arr_3, k);
+	System.out.println("After Left Rotation : " + Arrays.toString(arr_3)); // Expected Output = 4, 5, 6, 7, 1, 2, 3
+
+	int arr_4[] = { -1, -100, 3, 99 };
+	k = 2;
+	obj.leftRotate(arr_4, k);
+	System.out.println("After Left Rotation : " + Arrays.toString(arr_4)); // Expected Output = 3, 99, -1, -100
     }
 
+    /**
+     *
+     * @param nums
+     * @param k
+     *
+     *             Right Shifting Array
+     */
+    public void rotate(int[] nums, int k) {
+	if (nums == null || nums.length == 0 || k == 0) {
+	    return;
+	}
+	reverseArray(nums, 0, nums.length - 1); // Reverse full Array
+	reverseArray(nums, 0, k - 1); // Reverse from start to k-1
+	reverseArray(nums, k, nums.length - 1); // Reverse from k to end of array
+    }
+
+    /**
+     *
+     * @param nums
+     * @param start
+     * @param end
+     */
+    private void reverseArray(int[] nums, int start, int end) {
+	while (start < end) {
+	    int temp = nums[start];
+	    nums[start] = nums[end];
+	    nums[end] = temp;
+	    start++;
+	    end--;
+	}
+    }
+
+    /**
+     *
+     * @param nums
+     * @param k
+     *
+     * Left Rotate
+     */
+    public void leftRotate(int[] nums, int k) {
+	reverseArray(nums, 0, k - 1); // Reverse from start to k-1
+	reverseArray(nums, k, nums.length - 1); // Reverse from start to k-1
+	reverseArray(nums, 0, nums.length - 1); // Reverse full Array
+    }
 }
