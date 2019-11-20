8c37c77533bb60985f41d57343f4ebb3a117b4cb
sivasai-mudigonda <42163871+sivasai-mudigonda@users.noreply.github.com>
2019-11-19 21:03:08 -0800

    // Update LRUCache Implementation.

--- javapractice/src/leetcode/LRUCache.java
+++ javapractice/src/leetcode/LRUCache.java
@@ -6,6 +6,8 @@ package leetcode;
 import java. util. Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.HashMap;
+
 /**
  * @author SivaM
  * 
@@ -36,18 +38,145 @@ import java.util.Map;
 	cache.get(4);       // returns 4
 
  * 
+ * 
+ * Using QUEUE AND HashMap
+ * Time Complexity for put and get is O(1). This is because of put and get functions of hashmap.
+ * Space Complexity = O(n). This is because of Double linked list.
+ * 
+ * For Solution using Queue and HashMap, refer below links.
+ * Refer "https://www.youtube.com/watch?v=S6IfqDXWa10" {For Concept}
+ * Refer "https://github.com/bephrem1/backtobackswe/blob/master/Linked%20Lists/LRUCache.java" {For Code}
+ * 
+ * Using Linked-HashMap
  * Time Complexity = O(1)
  * Space Complexity = o(N) where N is the elements in the map
+ * 
  */
 public class LRUCache {
-
+	
+	// Implementation of LRU using QUEUE AND HASH-MAP
 	/**
 	 * @param args
 	 */
+	class DNode{
+		int key;
+		int value;
+		DNode prev;
+		DNode next;
+	}
+
+
+	class LRUCacheUtil{
+		int totalElementsInCache;
+		int totalCapacity;
+		DNode head,tail;
+		Map<Integer,DNode> lruMap= new HashMap<>();
+		
+		LRUCacheUtil(int capacity){
+			totalCapacity=capacity;
+			head= new DNode();
+			head.prev=null;
+			
+			tail= new DNode();
+			tail.next=null;
+			
+			head.next=tail;
+			tail.prev=head;
+		}
+		
+		public int get(int key) {
+			DNode node=lruMap.get(key);
+			if(node==null) {
+				return -1;
+			} else {
+				// Mode the node to the front
+				removeNode(node);
+				InsertNode(node);
+			}
+			return node.value;
+		}
+		
+		private void InsertNode(DNode node) {
+			// Insert from left{Queue}
+			node.prev=head; // point to dummy head
+			node.next=head.next; // Point to next item from the current head
+			
+			head.next.prev=node; // current heads, next item's, prev need to point to node.
+			
+			head.next=node; // Dummy Head next should point to node.
+			totalElementsInCache++;
+			
+		}
+		
+		private void removeNode(DNode node) {
+			DNode prevNode= node.prev;
+			DNode nextNode= node.next;
+			
+			prevNode.next=nextNode;
+			nextNode.prev=prevNode;
+			totalElementsInCache--;
+		}
+		
+		public void put(int key, int value) {
+			DNode node=lruMap.get(key);
+			if(node==null) {
+				// Insert new node
+				if(totalElementsInCache>=totalCapacity ) {
+					// Eviction Policy
+					removeFromTail();				
+				}
+				node = new DNode();
+				node.key=key;
+				node.value=value;
+			} else {
+				// Update value
+				removeNode(node);
+				node.value=value;			
+			}
+			InsertNode(node);
+			lruMap.put(key, node);
+		}
+		
+		private void removeFromTail() {
+			DNode node=tail.prev; // Dummy tail
+			removeNode(node);
+			lruMap.remove(node.key);
+		}
+	}
+
+
+	/**
+	 * Your LRUCache object will be instantiated and called as such:
+	 * LRUCache obj = new LRUCache(capacity);
+	 * int param_1 = obj.get(key);
+	 * obj.put(key,value);
+	 * @throws IllegalAccessException 
+	 * @throws InstantiationException 
+	 */
+
+	//********************************************************************************
 	
-	Map<Integer,Integer> map;
-	Integer capacity;
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InstantiationException, IllegalAccessException {
+		System.out.println("USING HASH-MAP AND QUEUE");
+		LRUCacheUtil lru= LRUCache.class.newInstance().new LRUCacheUtil(5); // 5 here is the capacity
+		lru.put(11,24); // Insert
+		lru.put(12,36); // Insert
+		int res = lru.get(11); // 11 key should come to front.
+		System.out.println("result = "+res);
+		lru.put(13,87); // Insert
+		lru.put(14,111); // Insert
+		lru.put(15,765); // Insert
+		lru.put(16,999); // Crossed existing capacity, should remove from tail{11} and insert new node{16} in front
+		lru.put(12,56); // Update Existing key's value and bring it to front
+		res = lru.get(11); // Key not found {-1}
+		System.out.println("result = "+res);
+		res = lru.get(16); // Output 999
+		System.out.println("result = "+res);
+		
+		//********************************************************************************
+		System.out.println("********************************************************************************");
+		System.out.println("USING LINKED-HASH-MAP");
+		
 		LRUCache cache = new LRUCache(2); /* capacity */
 		cache.put(1, 1);
 		cache.put(2, 2);
@@ -59,6 +188,10 @@ public class LRUCache {
 		System.out.println(cache.get(3));       // Expected returns 3
 		System.out.println(cache.get(4));       // Expected returns 4
 	}
+	
+	// Implementation of LRU using LINKED-HASH-MAP
+	Map<Integer,Integer> map;
+	Integer capacity;
 	
 	/**
 	 * 
