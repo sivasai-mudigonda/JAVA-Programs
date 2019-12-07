0a1aff7056bad0e14aff7dd1cbe4534b379c3b66
sivasai-mudigonda <42163871+sivasai-mudigonda@users.noreply.github.com>
2019-12-07 15:16:02 -0800

    // Update Leetcode solution for TwoSum

--- javapractice/src/leetcode/TwoSum.java
+++ javapractice/src/leetcode/TwoSum.java
@@ -1,7 +1,27 @@
 package leetcode;
 
 import java.util.HashMap;
+import java.util.Map;
 
+/**
+ * 
+ * @author SivaM
+ * 
+ * Leet-Code Ques - 1 {Two Sum} {Easy}
+ * https://leetcode.com/problems/two-sum/
+ * 
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * 
+	Example:
+	Given nums = [2, 7, 11, 15], target = 9,
+	Because nums[0] + nums[1] = 2 + 7 = 9,
+	return [0, 1].
+ *
+ * Time Complexity: O(N)
+ * Space Complexity: o(N)
+ *
+ */
 public class TwoSum {
 
 	public static void main(String[] args) {
@@ -15,19 +35,36 @@ public class TwoSum {
 		System.out.println(res[0]+","+res[1]);
 	}
 	
+	/**
+	 * 
+	 * @param nums
+	 * @param target
+	 * @return
+	 */
 	public int[] twoSumOnePass(int[] nums, int target) {
-		HashMap<Integer,Integer> hm= new HashMap<Integer, Integer>();
+		Map<Integer,Integer> map= new HashMap<Integer, Integer>();
 		for(int i=0;i<nums.length;i++) {
 			int compliment= target - nums[i];
-			if(hm.containsKey(compliment)) {
-				return new int[] {hm.get(compliment),i};
+			if(map.containsKey(compliment)) {
+				return new int[] {map.get(compliment),i};
 			}
-			hm.put(compliment, i);
+			map.put(nums[i], i);
 		}
-		throw new IllegalArgumentException("No two sum solution");
+		return new int[0];
 	}
 	
-	// Brut force-- without using compliment
+	/**
+	 * 
+	 * @param nums
+	 * @param target
+	 * @return
+	 * 
+	 * Brut force-- without using compliment
+	 * 
+	 * Time Complexity = O(N^2)
+	 * Space Complexity = o(1)
+	 * 
+	 */
 	public int[] twoSum(int[] nums, int target) {
         for(int i=0;i<nums.length;i++){
             for(int j=0;j<nums.length;j++){
